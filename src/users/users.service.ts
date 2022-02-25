228f8a52594a6fe1dddca1ddcf0c79251cbb7070
정세영 <wjdtp93@gmail.com>
2022-02-25 12:06:47 +0900

    Add send auth mail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,11 @@
 import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
+import { EmailService } from '../email/email.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private emailService: EmailService) {}
+
   async createUser(name: string, email: string, password: string) {
     await this.checkUserExists(email);
 
@@ -10,6 +13,14 @@ export class UsersService {
 
     await this.saveUser(name, email, password, signupVerifyToken);
     await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  async verifyEmail(signupVerifyToken: string) {
+    // TODO:
+    // 1. DB에서 signupVerifyToken으로 회원 가입 인증 유저인지 확인 후 처리
+    // 2. 바로 로그인 상태 되도록 JWT 발급
+    throw new Error('Method not implemented');
+    return '';
   }
 
   // TODO: DB 연동 후 구현
